ec20c4edb8f696759dde0a6875a41ee31ae681aa
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-12-28 13:13:21 +0300

    Load user info from server

--- app/users/user/route.js
+++ app/users/user/route.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        return Ember.RSVP.resolve({
-            id: 0,
-            name: 'VASILIY'
-        });
+    model({ user_id }) {
+        // const  user_id = param.user_id;
+        return Ember.$.getJSON(`https://jsonplaceholder.typicode.com/users/${user_id}`);
     }
 });
+
+// var p = { id: 1 };
+//
+// var id = p.id;
+// var { id } = p;
+// var { id } = { id: 1};
