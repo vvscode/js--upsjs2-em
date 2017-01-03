08e85457952fec3b4e8f9d292feb82adede17f20
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-01-04 01:54:02 +0300

    Separate templates for user /users.list

--- app/users/user/route.js
+++ app/users/user/route.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model({ user_id }) {
         // const  user_id = param.user_id;
-        return Ember.$.getJSON(`https://jsonplaceholder.typicode.com/users/${user_id}`);
+        return Ember.$.getJSON(`https://jsonplaceholder.typicode.com/users/${user_id}`)
+          .then((user) => ({
+            user,
+            users: this.modelFor('users')
+          }));
     },
 });
 
